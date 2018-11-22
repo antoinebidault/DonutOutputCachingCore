081ce322e7773a24c60f0301c7f4332bb26fb45f
Antoine Bidault <antoinebidault@outlook.com>
2018-11-22 15:08:19 +0100

    Clear the <donutoutputcaching> from response

--- src/CacheHoleHandler/DonutOutputCacheHandler.cs
+++ src/CacheHoleHandler/DonutOutputCacheHandler.cs
@@ -1,62 +1,86 @@
-﻿using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DonutOutputCachingCore.CacheHoleAttribute;
 using HtmlAgilityPack;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
-using DonutOutputCachingCore.CacheHoleAttribute;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Microsoft.AspNetCore.Mvc
 {
-    internal class DonutOutputCacheHandler
+  internal class DonutOutputCacheHandler
+  {
+    private ViewComponentHelperFactory _factory;
+    public DonutOutputCacheHandler(ViewComponentHelperFactory factory)
     {
-        private ViewComponentHelperFactory _factory;
-        public DonutOutputCacheHandler(ViewComponentHelperFactory factory)
-        {
-            _factory = factory;
-        }
+      _factory = factory;
+    }
 
-        internal IViewComponentHelper GetViewComponentHelper(HttpContext context)
-        {
-            return _factory.Create(context);
-        }
+    internal IViewComponentHelper GetViewComponentHelper(HttpContext context)
+    {
+      return _factory.Create(context);
+    }
 
-        internal async Task<byte[]> ParseAndExecuteChildRequestAsync(HttpContext context, byte[] htmlBytes)
-        {
-            object argsObject;
-            HtmlDocument htmlDoc;
-            string htmlString = System.Text.Encoding.UTF8.GetString(htmlBytes);
-            htmlDoc = new HtmlDocument();
-            htmlDoc.LoadHtml(htmlString);
-            var nodes = htmlDoc.DocumentNode.SelectNodes("descendant::donutoutputcache");
+    internal async Task<byte[]> ParseAndRemoveTheOutputCacheTags( byte[] htmlBytes)
+    {
+      string htmlString = System.Text.Encoding.UTF8.GetString(htmlBytes);
+      HtmlDocument htmlDoc = LoadDocument(htmlString);
+      var nodes = htmlDoc.DocumentNode.SelectNodes("descendant::donutoutputcache");
+      if (nodes == null || nodes.Count == 0)
+        return htmlBytes;
 
-            if (nodes == null || nodes.Count == 0)
-                return htmlBytes;
+      foreach (var node in nodes)
+      {
+        node.ParentNode.RemoveChild(node, true);
+      }
 
-            var donutHoles = new List<DonutHoleComponent>();
-            var helper = GetViewComponentHelper(context);
-
-            foreach (HtmlNode node in nodes)
-            {
-                var name = node.Attributes["data-name"].Value;
-                var args = node.Attributes["data-args"]?.Value;
-                argsObject = args != null ? JsonConvert.DeserializeObject(args) : null;
-
-                var result = await helper.InvokeAsync(name, arguments: argsObject);
-
-                htmlString = htmlString.Replace(node.InnerHtml, result?.GetString());
-            }
+      return await Task.FromResult(StringToByteArray(htmlDoc.DocumentNode.OuterHtml));
+    }
 
 
-            return await Task.FromResult(StringToByteArray(htmlString));
 
-        }
+    internal async Task<byte[]> ParseAndExecuteChildRequestAsync(HttpContext context, byte[] htmlBytes)
+    {
+      object argsObject;
+      string htmlString = System.Text.Encoding.UTF8.GetString(htmlBytes);
+      HtmlDocument htmlDoc = LoadDocument(htmlString);
+      var nodes = htmlDoc.DocumentNode.SelectNodes("descendant::donutoutputcache");
 
-        private byte[] StringToByteArray(string input)
-        {
-            return Encoding.ASCII.GetBytes(input);
-        }
+      if (nodes == null || nodes.Count == 0)
+        return htmlBytes;
+
+      var donutHoles = new List<DonutHoleComponent>();
+      var helper = GetViewComponentHelper(context);
+
+      foreach (HtmlNode node in nodes)
+      {
+        var name = node.Attributes["data-name"].Value;
+        var args = node.Attributes["data-args"]?.Value;
+        argsObject = args != null ? JsonConvert.DeserializeObject(args) : null;
+
+        var result = await helper.InvokeAsync(name, arguments: argsObject);
+
+        htmlString = htmlString.Replace(node.InnerHtml, result?.GetString());
+      }
+
+
+      return await Task.FromResult(StringToByteArray(htmlString));
 
     }
+
+    private static HtmlDocument LoadDocument(string htmlString)
+    {
+      HtmlDocument htmlDoc;
+      htmlDoc = new HtmlDocument();
+      htmlDoc.LoadHtml(htmlString);
+      return htmlDoc;
+    }
+
+    private byte[] StringToByteArray(string input)
+    {
+      return Encoding.ASCII.GetBytes(input);
+    }
+
+  }
 }
--- src/OutputCacheMiddleware.cs
+++ src/OutputCacheMiddleware.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.WebUtilities;
-using Microsoft.Extensions.Primitives;
 using Microsoft.Net.Http.Headers;
-using System;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
@@ -12,141 +10,134 @@ using System.Threading.Tasks;
 
 namespace DonutOutputCachingCore
 {
-    internal class DonutOutputCacheMiddleware
+  internal class DonutOutputCacheMiddleware
+  {
+    private readonly RequestDelegate _next;
+    private readonly IOutputCachingService _cache;
+    private readonly OutputCacheOptions _options;
+    private readonly DonutOutputCacheHandler _donutCacheHandler;
+
+    public DonutOutputCacheMiddleware(RequestDelegate next, IOutputCachingService cache, OutputCacheOptions options, DonutOutputCacheHandler donutCacheHandler)
     {
-        private readonly RequestDelegate _next;
-        private readonly IOutputCachingService _cache;
-        private readonly OutputCacheOptions _options;
-        private readonly DonutOutputCacheHandler _donutCacheHandler;
-
-        public DonutOutputCacheMiddleware(RequestDelegate next, IOutputCachingService cache, OutputCacheOptions options, DonutOutputCacheHandler donutCacheHandler)
-        {
-            _next = next;
-            _cache = cache;
-            _donutCacheHandler = donutCacheHandler;
-            _options = options;
-        }
-
-        public async Task InvokeAsync(HttpContext context)
-        {
-            if (!_options.DoesRequestQualify(context))
-            {
-                await _next(context);
-            }
-            else if (_cache.TryGetValue(context.Request.Host + context.Request.Path, out OutputCacheResponseEntry entry) && entry.IsCached(context, out OutputCacheResponse item))
-            {
-                item.Body = await _donutCacheHandler.ParseAndExecuteChildRequestAsync(context,item.Body);
-                await ServeFromCacheAsync(context, item);
-            }
-            else
-            {
-                await ServeFromMvcAndCacheAsync(context, entry);
-            }
-        }
-
-        private async Task ServeFromMvcAndCacheAsync(HttpContext context, OutputCacheResponseEntry entry)
-        {
-            HttpResponse response = context.Response;
-            Stream originalStream = response.Body;
-
-            try
-            {
-                using (var ms = new MemoryStream())
-                {
-                    response.Body = ms;
-
-                    await _next(context);
-
-                    if (_options.DoesResponseQualify(context))
-                    {
-                        byte[] bytes = ms.ToArray();
-
-                        AddEtagToResponse(context, bytes);
-                        AddResponseToCache(context, entry, bytes);
-                    }
-
-                    if (ms.Length > 0)
-                    {
-                        ms.Seek(0, SeekOrigin.Begin);
-
-                        await ms.CopyToAsync(originalStream);
-                    }
-                }
-            }
-            finally
-            {
-                response.Body = originalStream;
-            }
-        }
-
-        private static async Task ServeFromCacheAsync(HttpContext context, OutputCacheResponse value)
-        {
-            /*
-            // Copy over the HTTP headers
-            foreach (string name in value.Headers.Keys)
-            {
-                if (!context.Response.Headers.ContainsKey(name))
-                {
-                    context.Response.Headers[name] = value.Headers[name];
-                }
-            }
-
-            // Serve a conditional GET request when if-none-match header exist
-            if (context.Request.Headers.TryGetValue(HeaderNames.IfNoneMatch, out StringValues etag) && context.Response.Headers[HeaderNames.ETag] == etag)
-            {
-                await context.Response.Body.WriteAsync(value.Body, 0, value.Body.Length);
-                context.Response.ContentLength = 0;
-                context.Response.StatusCode = StatusCodes.Status304NotModified;
-            }
-            else
-            {*/
-                await context.Response.Body.WriteAsync(value.Body, 0, value.Body.Length);
-         //   }
-        }
-
-        private void AddResponseToCache(HttpContext context, OutputCacheResponseEntry entry, byte[] bytes)
-        {
-            if (!context.IsOutputCachingEnabled(out OutputCacheProfile profile))
-            {
-                return;
-            }
-
-            if (entry == null)
-            {
-                entry = new OutputCacheResponseEntry(context, bytes, profile);
-                _cache.Set(context.Request.Host + context.Request.Path, entry, context);
-            }
-            else
-            {
-                entry.AddResponse(context, new OutputCacheResponse(bytes, context.Response.Headers));
-            }
-        }
-
-        private static void AddEtagToResponse(HttpContext context, byte[] bytes)
-        {
-            if (context.Response.StatusCode != StatusCodes.Status200OK)
-                return;
-
-            if (!context.IsOutputCachingEnabled(out OutputCacheProfile profile))
-            {
-                return;
-            }
-
-            if (context.Response.Headers.ContainsKey(HeaderNames.ETag))
-                return;
-
-            context.Response.Headers[HeaderNames.ETag] = CalculateChecksum(bytes, context.Request);
-        }
-
-        private static string CalculateChecksum(byte[] bytes, HttpRequest request)
-        {
-            byte[] encoding = Encoding.UTF8.GetBytes(request.Headers[HeaderNames.AcceptEncoding].ToString());
-
-            using (var algo = SHA1.Create())
-            {
-                byte[] buffer = algo.ComputeHash(bytes.Concat(encoding).ToArray());
-                return $"\"{WebEncoders.Base64UrlEncode(buffer)}\"";
-            }
-        }
+      _next = next;
+      _cache = cache;
+      _donutCacheHandler = donutCacheHandler;
+      _options = options;
     }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+      if (!_options.DoesRequestQualify(context))
+      {
+        await _next(context);
+      }
+      else if (_cache.TryGetValue(context.Request.Host + context.Request.Path, out OutputCacheResponseEntry entry) && entry.IsCached(context, out OutputCacheResponse item))
+      {
+
+        item.Body = await _donutCacheHandler.ParseAndExecuteChildRequestAsync(context, item.Body);
+        await ServeFromCacheAsync(context, item);
+      }
+      else
+      {
+        await ServeFromMvcAndCacheAsync(context, entry);
+      }
+    }
+
+    private async Task ServeFromMvcAndCacheAsync(HttpContext context, OutputCacheResponseEntry entry)
+    {
+      HttpResponse response = context.Response;
+      Stream originalStream = response.Body;
+
+      try
+      {
+        using (var ms = new MemoryStream())
+        {
+          response.Body = ms;
+
+          await _next(context);
+
+          if (_options.DoesResponseQualify(context))
+          {
+            byte[] bytes = ms.ToArray();
+
+            AddEtagToResponse(context, bytes);
+            AddResponseToCache(context, entry, bytes);
+          }
+
+          var responseWithoutDonutTags = await _donutCacheHandler.ParseAndRemoveTheOutputCacheTags(ms.ToArray());
+          using (var tempStream = new MemoryStream(responseWithoutDonutTags))
+          {
+            tempStream.Seek(0, SeekOrigin.Begin);
+            await tempStream.CopyToAsync(originalStream);
+          }
+        }
+      }
+      finally
+      {
+        response.Body = originalStream;
+      }
+    }
+
+    private async Task ServeFromCacheAsync(HttpContext context, OutputCacheResponse value)
+    {
+
+      // Copy over the HTTP headers
+      foreach (string name in value.Headers.Keys)
+      {
+        if (!context.Response.Headers.ContainsKey(name))
+        {
+          context.Response.Headers[name] = value.Headers[name];
+        }
+      }
+
+      var body = await _donutCacheHandler.ParseAndRemoveTheOutputCacheTags(value.Body);
+      context.Response.ContentLength = body.Length;
+      await context.Response.Body.WriteAsync(body, 0, body.Length);
+    }
+
+    private void AddResponseToCache(HttpContext context, OutputCacheResponseEntry entry, byte[] bytes)
+    {
+      if (!context.IsOutputCachingEnabled(out OutputCacheProfile profile))
+      {
+        return;
+      }
+
+      if (entry == null)
+      {
+        entry = new OutputCacheResponseEntry(context, bytes, profile);
+        _cache.Set(context.Request.Host + context.Request.Path, entry, context);
+      }
+      else
+      {
+        entry.AddResponse(context, new OutputCacheResponse(bytes, context.Response.Headers));
+      }
+    }
+
+    private static void AddEtagToResponse(HttpContext context, byte[] bytes)
+    {
+      if (context.Response.StatusCode != StatusCodes.Status200OK)
+        return;
+
+      if (!context.IsOutputCachingEnabled(out OutputCacheProfile profile))
+      {
+        return;
+      }
+
+      if (context.Response.Headers.ContainsKey(HeaderNames.ETag))
+        return;
+
+      context.Response.Headers[HeaderNames.ETag] = CalculateChecksum(bytes, context.Request);
+    }
+
+    private static string CalculateChecksum(byte[] bytes, HttpRequest request)
+    {
+      byte[] encoding = Encoding.UTF8.GetBytes(request.Headers[HeaderNames.AcceptEncoding].ToString());
+
+      using (var algo = SHA1.Create())
+      {
+        byte[] buffer = algo.ComputeHash(bytes.Concat(encoding).ToArray());
+        return $"\"{WebEncoders.Base64UrlEncode(buffer)}\"";
+      }
+    }
+  }
 }
