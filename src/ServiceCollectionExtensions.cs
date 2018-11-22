1148bf458b8fab2c462a6d7b15d9e31ec3eaf7b3
Antoine Bidault <antoinebidault@outlook.com>
2018-11-22 15:44:22 +0100

    Corrected the AddDonutOutputCaching() serviceCollection extension method

--- src/ServiceCollectionExtensions.cs
+++ src/ServiceCollectionExtensions.cs
@@ -1,49 +1,43 @@
-﻿using System;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ViewComponents;
-using Microsoft.AspNetCore.Mvc.ViewFeatures.Internal;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Net.Http.Headers;
-using DonutOutputCachingCore;
+﻿using DonutOutputCachingCore;
 using DonutOutputCachingCore.CacheHoleAttribute;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
+  /// <summary>
+  /// Extension methods to register the output caching service.
+  /// </summary>
+  public static class ServiceCollectionExtensions
+  {
     /// <summary>
-    /// Extension methods to register the output caching service.
+    /// Registers the output caching service with the dependency injection system.
     /// </summary>
-    public static class ServiceCollectionExtensions
+    public static void AddDonutOutputCaching(this IServiceCollection services)
     {
-        /// <summary>
-        /// Registers the output caching service with the dependency injection system.
-        /// </summary>
-        public static void AddOutputCaching(this IServiceCollection services)
-        {
-            var options = new OutputCacheOptions();
+      var options = new OutputCacheOptions();
 
-            services.AddSingleton(options);
-            //   services.AddTransient<IViewBufferScope, MemoryPoolViewBufferScope>();
-            //services.AddTransient<IViewComponentHelper, DefaultViewComponentHelper>();
-            services.AddSingleton<ViewComponentHelperFactory>();
-            services.AddTransient<DonutOutputCacheHandler>();
-            services.AddSingleton<IOutputCachingService, OutputCachingService>();
-        }
-
-        /// <summary>
-        /// Registers the output caching service with the dependency injection system.
-        /// </summary>
-        public static void AddDonutOutputCaching(this IServiceCollection services, Action<OutputCacheOptions> outputCacheOptions)
-        {
-            var options = new OutputCacheOptions();
-            outputCacheOptions(options);
-
-            services.AddSingleton(options);
-            services.AddSingleton<ViewComponentHelperFactory>();
-            services.AddTransient<IViewBufferScope, MemoryPoolViewBufferScope>();
-            services.AddTransient<IViewComponentHelper, DefaultViewComponentHelper>();
-            services.AddTransient<DonutOutputCacheHandler>();
-            services.TryAddSingleton<IOutputCachingService, OutputCachingService>();
-        }
+      services.AddSingleton(options);
+      //   services.AddTransient<IViewBufferScope, MemoryPoolViewBufferScope>();
+      //services.AddTransient<IViewComponentHelper, DefaultViewComponentHelper>();
+      services.AddSingleton<ViewComponentHelperFactory>();
+      services.AddSingleton<DonutOutputCacheHandler>();
+      services.AddSingleton<IOutputCachingService, OutputCachingService>();
     }
+
+    /// <summary>
+    /// Registers the output caching service with the dependency injection system.
+    /// </summary>
+    public static void AddDonutOutputCaching(this IServiceCollection services, Action<OutputCacheOptions> outputCacheOptions)
+    {
+      var options = new OutputCacheOptions();
+      outputCacheOptions(options);
+
+      services.AddSingleton(options);
+      services.AddSingleton<ViewComponentHelperFactory>();
+      services.AddSingleton<DonutOutputCacheHandler>();
+      services.TryAddSingleton<IOutputCachingService, OutputCachingService>();
+    }
+  }
 }
